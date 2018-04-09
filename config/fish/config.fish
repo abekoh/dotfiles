ee46d1eb44c7dcf9a938a316c6061426a780afbb
abekoh <abekoh.bcky@gmail.com>
2018-04-09 21:46:19 +0900

    [update] remove some lines of config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,22 +10,8 @@ set -x WORKON_HOME ~/.virtualenvs
 set -x EDITOR nvim
 set -x TERM xterm-256color
 
-switch (uname)
-case Darwin
-  set -x PYENV_ROOT $HOME/.pyenv
-  set -x PATH $HOME/.nodebrew/current/bin $PATH
-case Linux
-  set -x LD_LIBRARY_PATH /usr/local/cuda/lib64:/usr/local/lib
-  set -x CUDA_HOME /usr/local/cuda
-  set -x PATH /usr/local/cuda/bin $PATH
-end
-
 # alias
 alias vi='vim -u NONE -N'
-switch (uname)
-case Darwin
-  alias rm='rmtrash'
-end
 
 # key-bind
 bind \cl accept-autosuggestion
