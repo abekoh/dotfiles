e46c2dfc8c878c82611d9da69349d11e2e810808
Kotaro Abe <abekoh.bcky@gmail.com>
2018-04-15 01:52:04 +0900

    [update] install boost

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ if [ "$(uname)" == 'Darwin' ]; then
     brew install fish neovim python3 tmux reattach-to-user-namespace ripgrep boost
     brew install vim --with-override-system-vi
 # Ubuntu
-# TODO: install ripgrep, boost
+# TODO: install ripgrep
 elif [ "$(uname)" == 'Linux' ] && [ -e /etc/lsb-release ]; then
     sudo apt-add-repository -y ppa:fish-shell/release-2
     sudo apt-add-repository -y ppa:neovim-ppa/unstable
     sudo apt-add-repository -y ppa:jonathonf/vim
     sudo apt -y update
-    sudo apt -y install fish mercurial software-properties-common vim neovim python-dev python-pip python3-dev python3-pip tmux curl
+    sudo apt -y install fish mercurial software-properties-common vim neovim python-dev python-pip python3-dev python3-pip tmux curl libboost-all-dev
 # CentOS (>=7)
-# TODO: install ripgrep, boost
+# TODO: install ripgrep
 elif [ "$(uname)" == 'Linux' ] && [ -e /etc/centos-release ] && [ "$(cat /etc/redhat-release | sed -e 's/.*\s\([0-9]\)\..*/\1/')" == '7' ]; then
     yum install -y https://centos7.iuscommunity.org/ius-release.rpm
     yum remove -y vim*
@@ -22,7 +22,9 @@ elif [ "$(uname)" == 'Linux' ] && [ -e /etc/centos-release ] && [ "$(cat /etc/re
     curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
     curl -o /etc/yum.repos.d/mcepl-vim8-epel-7.repo https://copr.fedorainfracloud.org/coprs/mcepl/vim8/repo/epel-7/mcepl-vim8-epel-7.repo
     wget -O /etc/yum.repos.d/shells:fish:release:2.repo https://download.opensuse.org/repositories/shells:fish:release:2/CentOS_7/shells:fish:release:2.repo
-    yum install -y vim neovim fish tmux python36u python36u-libs python36u-devel python36u-pip python2-pip
+    wget -O /etc/yum.repos.d/enetres.repo http://repo.enetres.net/enetres.repo
+    yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+    yum install -y vim neovim fish tmux python36u python36u-libs python36u-devel python36u-pip python2-pip boost-devel ripgrep
     ln -sf /usr/bin/python3.6 /usr/bin/python3
     ln -sf /usr/bin/pip3.6 /usr/bin/pip3
 else
