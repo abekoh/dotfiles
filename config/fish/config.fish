cd3bbc9dee5a7d38fabc30ba4eb13a12ab0f242d
Kotaro Abe <abekoh.bcky@gmail.com>
2018-04-25 22:53:24 +0900

    [update] change fish settings

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,20 +1,12 @@
-# vi mode
-# fish_vi_key_bindings
-# function fish_mode_prompt 
-# end
-set -g fish_key_bindings fish_default_key_bindings
-
 # environment vars
 set -x PATH /usr/local/bin $PATH
 set -x WORKON_HOME ~/.virtualenvs
 set -x EDITOR nvim
 set -x TERM xterm-256color
+set -x GOPATH ~/.go
 
 # alias
 alias vi='vim -u NONE -N'
-
-# key-bind
-bind \cl accept-autosuggestion
 
 # complete
 complete -c workon -a "(ls $WORKON_HOME)"
--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,6 @@
 function fish_user_key_bindings
-  bind \cl accept-autosuggestion
+  for mode in insert default visual
+    fish_default_key_bindings -M $mode
+  end
+  fish_vi_key_bindings --no-erase
 end
