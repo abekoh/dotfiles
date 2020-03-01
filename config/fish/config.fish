c5327fbac9da1380fe23c8d0041ae5bbcabed240
Kotaro Abe <abekoh.bcky@gmail.com>
2020-03-01 15:15:51 +0900

    use anyenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,13 @@
 # environment vars
 set -x PATH /usr/local/bin $PATH
-set -x WORKON_HOME ~/.virtualenvs
 set -x EDITOR nvim
 set -x TERM xterm-256color
 set -x GOPATH ~
-set -x PATH ~/.nodebrew/current/bin $PATH
 set -x PATH ~/bin $PATH
 set -x NRFSDK12_ROOT ~/sdk/nRF5_SDK_12.3.0_d7731ad
-set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -x PATH $JAVA_HOME/bin $PATH
+# set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+# set -x PATH $JAVA_HOME/bin $PATH
 set -x PATH ~/google-cloud-sdk/bin $PATH
-set -x PATH ~/.jenv/bin $PATH
-set -x PATH ~/.nodenv/shims $PATH
 
 # alias
 alias vi='vim -u NONE -N'
@@ -24,7 +20,4 @@ alias k='kubectl'
 complete -c workon -a "(ls $WORKON_HOME)"
 
 # run
-eval (python3 -m virtualfish)
-source (rbenv init - | psub)
-source (jenv init - | psub)
-status --is-interactive; and source (nodenv init -|psub)
+status --is-interactive; and source (anyenv init -|psub)
