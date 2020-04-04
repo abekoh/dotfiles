be562f9cb052944d1a71139e6fa347c0f3b3e06d
Kotaro Abe <abekoh.bcky@gmail.com>
2020-04-04 14:53:51 +0900

    add rust path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,7 @@ set -x NRFSDK12_ROOT ~/sdk/nRF5_SDK_12.3.0_d7731ad
 # set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
 # set -x PATH $JAVA_HOME/bin $PATH
 set -x PATH ~/google-cloud-sdk/bin $PATH
+set -x PATH ~/.cargo/bin $PATH
 
 # alias
 alias vi='vim -u NONE -N'
