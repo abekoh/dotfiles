9618265a5926ef5b45adbb030204a15a34a3ec72
Kotaro Abe <abekoh.bcky@gmail.com>
2023-08-30 00:20:29 +0900

    update

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -5,5 +5,18 @@ return {
     {
       "jacoborus/tender.vim",
     },
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      config = function()
+        require("copilot").setup({
+          suggestion = {
+            enabled = true,
+            auto_trigger = true,
+          }
+        })
+      end,
+    },
   },
 }
