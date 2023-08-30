8e380b5676f771647b33e6cd4387ed2d36816b66
Kotaro Abe <abekoh.bcky@gmail.com>
2023-08-31 01:12:30 +0900

    update

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -21,6 +21,14 @@ return {
 
   plugins = {
     {
+      "goolord/alpha-nvim",
+      opts = function()
+        local dashboard = require("alpha.themes.dashboard")
+        dashboard.section.header.val = {}
+        dashboard.section.buttons.val = {}
+      end
+    },
+    {
       "williamboman/mason-lspconfig.nvim",
       opts = function(_, opts)
         opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
