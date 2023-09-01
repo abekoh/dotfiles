b0420417c0f1941bdc433402f691d7afe8d6a5a2
Kotaro Abe <abekoh.bcky@gmail.com>
2023-09-01 22:17:43 +0900

    update

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -21,6 +21,10 @@ return {
 
   plugins = {
     {
+      "nvim-treesitter/nvim-treesitter",
+      auto_install = true,
+    },
+    {
       "goolord/alpha-nvim",
       opts = function()
         local dashboard = require("alpha.themes.dashboard")
@@ -117,30 +121,4 @@ return {
       "imsnif/kdl.vim",
     },
   },
-
-  -- https://astronvim.com/Recipes/yaml_ansible
-  -- polish = function()
-  --   local function yaml_ft(path, bufnr)
-  --     -- get content of buffer as string
-  --     local content = vim.filetype.getlines(bufnr)
-  --     if type(content) == "table" then content = table.concat(content, "\n") end
-  --
-  --     -- check if file is in roles, tasks, or handlers folder
-  --     local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-  --     if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
-  --     -- check for known ansible playbook text and if found, return yaml.ansible
-  --     local regex = vim.regex "hosts:\\|tasks:"
-  --     if regex and regex:match_str(content) then return "yaml.ansible" end
-  --
-  --     -- return yaml if nothing else
-  --     return "yaml"
-  --   end
-  --
-  --   vim.filetype.add {
-  --     extension = {
-  --       yml = yaml_ft,
-  --       yaml = yaml_ft,
-  --     },
-  --   }
-  -- end,
 }
