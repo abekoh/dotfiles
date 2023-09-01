33b3f9d5ed9845a0eccc789191c73533cd8a84fd
Kotaro Abe <abekoh.bcky@gmail.com>
2023-09-01 22:02:28 +0900

    update

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -32,7 +32,7 @@ return {
       "williamboman/mason-lspconfig.nvim",
       opts = function(_, opts)
         opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-          "ansiblels",
+          -- "ansiblels",
           "astro",
           "bashls",
           "clangd",
@@ -119,28 +119,28 @@ return {
   },
 
   -- https://astronvim.com/Recipes/yaml_ansible
-  polish = function()
-    local function yaml_ft(path, bufnr)
-      -- get content of buffer as string
-      local content = vim.filetype.getlines(bufnr)
-      if type(content) == "table" then content = table.concat(content, "\n") end
-
-      -- check if file is in roles, tasks, or handlers folder
-      local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-      if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
-      -- check for known ansible playbook text and if found, return yaml.ansible
-      local regex = vim.regex "hosts:\\|tasks:"
-      if regex and regex:match_str(content) then return "yaml.ansible" end
-
-      -- return yaml if nothing else
-      return "yaml"
-    end
-
-    vim.filetype.add {
-      extension = {
-        yml = yaml_ft,
-        yaml = yaml_ft,
-      },
-    }
-  end,
+  -- polish = function()
+  --   local function yaml_ft(path, bufnr)
+  --     -- get content of buffer as string
+  --     local content = vim.filetype.getlines(bufnr)
+  --     if type(content) == "table" then content = table.concat(content, "\n") end
+  --
+  --     -- check if file is in roles, tasks, or handlers folder
+  --     local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
+  --     if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+  --     -- check for known ansible playbook text and if found, return yaml.ansible
+  --     local regex = vim.regex "hosts:\\|tasks:"
+  --     if regex and regex:match_str(content) then return "yaml.ansible" end
+  --
+  --     -- return yaml if nothing else
+  --     return "yaml"
+  --   end
+  --
+  --   vim.filetype.add {
+  --     extension = {
+  --       yml = yaml_ft,
+  --       yaml = yaml_ft,
+  --     },
+  --   }
+  -- end,
 }
--- config/zsh/.zshrc
+++ config/zsh/.zshrc
@@ -44,6 +44,9 @@ export HISTSIZE=10000
 export GOPATH=$(go env GOPATH)
 export GOBIN=$(go env GOBIN)
 
+# ignore ctrl-d
+set -o ignoreeof
+
 ### Added by Zinit's installer
 if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
     print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
