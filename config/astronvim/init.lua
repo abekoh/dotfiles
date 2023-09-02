c193aac376578a23f67df4c16156c41baba78d22
Kotaro Abe <abekoh.bcky@gmail.com>
2023-09-02 10:28:50 +0900

    update plugins

--- config/astronvim/init.lua
+++ config/astronvim/init.lua
@@ -106,6 +106,21 @@ return {
       end,
     },
     {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+    },
+    {
+      "t9md/vim-quickhl",
+      event = "VeryLazy",
+      config = function()
+        vim.api.nvim_set_keymap("n", "<space>m", "<Plug>(quickhl-manual-this)", {})
+        vim.api.nvim_set_keymap("x", "<Leader>m", "<Plug>(quickhl-manual-this)", { silent = true })
+        vim.api.nvim_set_keymap("n", "<Leader>M", "<Plug>(quickhl-manual-reset)", { silent = true })
+        vim.api.nvim_set_keymap("x", "<Leader>M", "<Plug>(quickhl-manual-reset)", { silent = true })
+      end
+    },
+    {
       "jay-babu/mason-nvim-dap.nvim",
       opts = {
         ensure_installed = { "python", "delve" }
