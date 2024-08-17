1c89c191c48defdf8c2f1bbcc91566027988a8a4
Kotaro Abe <abekoh.bcky@gmail.com>
2024-08-17 13:39:16 +0900

    remove

--- config/zsh/sync.zsh
+++ config/zsh/sync.zsh
@@ -1,18 +1,3 @@
-case ${OSTYPE} in
-  darwin*)
-    # homebrew
-    export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
-    export HOMEBREW_REPOSITORY="/opt/homebrew";
-    export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
-    export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
-    export HOMEBREW_FILE="~/dotfiles/Brewfile"
-    export FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-    ;;
-  linux*)
-    ;;
-esac
-
 # path
 export PATH=/usr/local/bin${PATH+:$PATH}
 export PATH=$HOME/bin${PATH+:$PATH}
