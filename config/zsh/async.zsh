b2875279e4fa6adf685330958e0617879f6e936c
Kotaro Abe <abekoh.bcky@gmail.com>
2026-01-24 12:19:58 +0900

    remove blocking

--- config/zsh/async.zsh
+++ config/zsh/async.zsh
@@ -1,6 +1,10 @@
 export GOPATH=$(go env GOPATH)
 export GOBIN=$(go env GOBIN)
-export GOTOOLCHAIN=$(curl -s -L "https://go.dev/VERSION?m=text" | head -n 1)+auto
+# GOTOOLCHAIN is set asynchronously to avoid blocking shell startup
+() {
+  local gotoolchain=$(curl -s -L "https://go.dev/VERSION?m=text" 2>/dev/null | head -n 1)
+  [[ -n "$gotoolchain" ]] && export GOTOOLCHAIN="${gotoolchain}+auto"
+} &!
 
 # direnv
 eval "$(direnv hook zsh)"
